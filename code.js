d6b4a42f2df01e7473c83d1bc8799385a4b8093b
Trent Oswald <trentoswald@therebelrobot.com>
2016-04-15 09:17:35 -0700

    slides

---
+++ code.js
@@ -0,0 +1,136 @@
+window.__code = 'function Contributing_to (openSource) {\n' +
+  '  // Trent Oswald \n' +
+  '  //    @therebelrobot \n' +
+  '  //    therebelrobot.com\n' +
+  '  // Dev Bootcamp, April 16th 2016\n' +
+  '\n' +
+  '  what_is(openSource);\n' +
+  '\n' +
+  '  (openSource !== freeSoftware);\n' +
+  '\n' +
+  '  openSource = "source code available to the general public with relaxed or non-existent copyright restrictions";\n' +
+  '\n' +
+  '  openSource = [\n' +
+  '    "Rich and Thriving Community",\n' +
+  '    "Shared Values",\n' +
+  '    "Open Governance",\n' +
+  '    "Ability and Freedom to Iterate",\n' +
+  '    "Passion for Innovation"\n' +
+  '  ]\n' +
+  '\n' +
+  '  openSource << FreeSoftwareFoundation(1997)\n' +
+  '\n' +
+  '  openSourceInitiative(1998)\n' +
+  '\n' +
+  '  Netscape() // Now Mozilla Firefox\n' +
+  '  Debian()\n' +
+  '  LinuxKernel()\n' +
+  '\n' +
+  '  openSource = [\n' +
+  '    "Media Content", // Wikimedia Foundation\n' +
+  '    "Hardware", // Raspberry Pi, Arduino\n' +
+  '    "Beverages",// Open Source Cola, Brewtopia\n' +
+  '    "Firearms", // 3D Printed firearms\n' +
+  '    "Medicine", // Tropical Disease Initiative\n' +
+  '    "Religion" // Open Siddur Project, Open Source Yoga Unity\n' +
+  '  ]\n' +
+  '\n' +
+  '  var whatMakesOpenSource = function () {\n' +
+  '    releaseOfSourceCode() \n' +
+  '            // Github, Gitlab, Bitbucket, etc\n' +
+  '    publicAndPermissiveLicense() \n' +
+  '            // MIT, ISC, GPLv3, Apache, etc\n' +
+  '    clearDocumentation() \n' +
+  '            // README, CONTRIBUTING, CHANGELOG\n' +
+  '  }\n' +
+  '\n' +
+  '  React // Facebook\n' +
+  '  RubyOnRails // David Hansson\n' +
+  '  Linux // The Linux Foundation\n' +
+  '  Android // Google\n' +
+  '  .NETframework // C# - Microsoft\n' +
+  '\n' +
+  '  var howDoIOpenSource = function () {\n' +
+  '    methods = [\n' +
+  '      "Start Your Own",\n' +
+  '      "Contribute to Others"\n' +
+  '    ]\n' +
+  '\n' +
+  '    if(method === "Start Your Own"){\n' +
+  '      releaseOfSourceCode()\n' +
+  '      publicAndPermissiveLicense()\n' +
+  '          // choosealicense.com, tldrlegal.com\n' +
+  '      clearDocumentation()\n' +
+  '         // ossmanifesto.org, c4sf.me/docsquad\n' +
+  '\n' +
+  '      testing() // Tap, Mocha, Nightwatch\n' +
+  '      publishing() // NPM, Gems, LuaRocks\n' +
+  '      evangelizing() \n' +
+  '           // Github, HackerNews, Reddit, IRC\n' +
+  '\n' +
+  '      if(broken) {\n' +
+  '        buildFix()\n' +
+  '        release\n' +
+  '      }\n' +
+  '\n' +
+  '      url("http://ossmanifesto.org")\n' +
+  '      values = [\n' +
+  '        "Respect",\n' +
+  '        "Collaboration",\n' +
+  '        "Healthy Debates",\n' +
+  '        "Basic Documentation",\n' +
+  '        "Consistent Versioning of releases"\n' +
+  '      ]\n' +
+  '      \n' +
+  '    }\n' +
+  '    if(method === "Contribute to Others") {\n' +
+  '      humanInteraction()\n' +
+  '\n' +
+  '      remember(`They\'re coders, \n' +
+  '                 just like you`)\n' +
+  '      remember(`Nobody is born a coder. \n' +
+  '                 We all start somewhere.`)\n' +
+  '      remember(`If you don\'t like a community,\n' +
+  '                 find (or build) a new one`)\n' +
+  '\n' +
+  '      contributionModels = [\n' +
+  '        "Branching", \n' +
+  '           // internal parallel code bases\n' +
+  '        "Forking" \n' +
+  '           // external parallel code base\n' +
+  '      ]\n' +
+  '      openSourceContributions === "Forking"\n' +
+  '\n' +
+  '      CONTRIBUTING.md()\n' +
+  '        // How to file an issue\n' +
+  '        // What to include\n' +
+  '        // How to contribute code\n' +
+  '\n' +
+  '      GithubIssues = [\n' +
+  '        "Ask for LICENSE (if none provided)",\n' +
+  '        "Report found bugs",\n' +
+  '        "Try to replicate logged bugs",\n' +
+  '        "Suggest feature additions"\n' +
+  '      ]\n' +
+  '      remember(`Language / culture / \n' +
+  '                   personality barriers`)\n' +
+  '      remember(`Response times vary \n' +
+  '                   dramatically`)\n' +
+  '\n' +
+  '      GithubForking = [\n' +
+  '        "Assist in documentation improvement",\n' +
+  '        "Assist in writing tests",\n' +
+  '        "Try fixing a currently reported bug",\n' +
+  '        "Try fixing a bug you found",\n' +
+  '        "Try adding a proposed feature",\n' +
+  '        "Try adding a feature you would like"\n' +
+  '        // WARNING: It is best practice \n' +
+  '        //          to check in with \n' +
+  '        //          a core contributor before\n' +
+  '        //          adding features.\n' +
+  '      ]\n' +
+  '\n' +
+  '      url("http://up-for-grabs.net")\n' +
+  '    }\n' +
+  '  }\n' +
+  '}'
---
+++ slides.js
@@ -0,0 +1,85 @@
+window.__slides = [
+  {
+    loc: [0, 5],
+    note: 'https://therebelrobot.com/open-source-workshop'
+  }, {
+    loc: [6, 7]
+  }, {
+    loc: [8, 9],
+    note: 'What Open Source Is Not'
+  }, {
+    loc: [10, 11]
+  }, {
+    loc: [12, 19]
+  }, {
+    loc: [20, 21],
+    note: 'Origins'
+  }, {
+    loc: [22, 27],
+    note: 'Origins'
+  }, {
+    loc: [28, 36],
+    note: 'Not just programming.'
+  }, {
+    loc: [37, 38]
+  }, {
+    loc: [38, 40],
+    note: 'Have a public Github account? Congrats!'
+  }, {
+    loc: [40, 42]
+  }, {
+    loc: [42, 44]
+  }, {
+    loc: [46, 51],
+    note: 'Current Open Source Projects'
+  }, {
+    loc: [52, 53]
+  }, {
+    loc: [52, 57]
+  }, {
+    loc: [58, 59]
+  }, {
+    loc: [59, 64]
+  }, {
+    loc: [65, 69]
+  }, {
+    loc: [70, 74],
+    note: 'The heart of open source'
+  }, {
+    loc: [75,83]
+  }, {
+    loc: [58, 69],
+    note: 'https://github.com/therebelrobot/open-source-workshop#start-your-own'
+  }, {
+    loc: [85,86]
+  }, {
+    loc: [86, 87],
+    note: 'much more daunting'
+  }, {
+    loc: [88, 94],
+    note: 'Interpersonal skillz ftw'
+  }, {
+    loc: [95,101]
+  }, {
+    loc: [101,102],
+    note: 'Most open source contributions'
+  }, {
+    loc: [103,107],
+    note: 'STOP! Before anything...'
+  }, {
+    loc: [108, 114],
+    note: 'No coding required!'
+  }, {
+    loc: [114,118]
+  }, {
+    loc: [119, 131],
+    note: 'No coding required!'
+  }, {
+    loc: [132, 133],
+    note: 'Issues all over Github that need help'
+  },{
+    loc: [0, 5],
+    title: 'Let\'s dive in!',
+    note: 'https://therebelrobot.com/open-source-workshop'
+  }
+]
